7ebd88d5cfd1eb92da7382598de69bef4117926a
Adi Chirilov <developer@rosoftware.ro>
2019-11-16 13:04:45 +0200

    Code and dependencies cleanup

--- app/models.py
+++ app/models.py
@@ -1,11 +1,7 @@
 # -*- encoding: utf-8 -*-
 """
-Argon Dashboard - coded in Flask
-
-Author  : AppSeed App Generator
-Design  : Creative-Tim.com
-License : MIT 
-Support : https://appseed.us/support 
+License: MIT
+Copyright (c) 2019 - present AppSeed.us
 """
 
 from app         import db
@@ -35,45 +31,3 @@ class User(UserMixin, db.Model):
         db.session.commit( )
 
         return self 
-
-class Stats(db.Model):
-
-    id    = db.Column(db.Integer,     primary_key=True)
-    key   = db.Column(db.String(64),  unique=True)
-    val   = db.Column(db.Integer)
-    val_s = db.Column(db.String(256))
-
-    def __init__(self, key):
-        self.key   = key
-
-        db_obj = Stats.query.filter_by(key=key).first()
-        if db_obj:
-            self.id    = db_obj.id
-            self.key   = db_obj.key
-            self.val   = db_obj.val
-            self.val_s = db_obj.val_s
-
-        else:
-
-            db.session.add ( self )
-
-            self.val   = 0
-            self.val_s = ''
-
-    def __repr__(self):
-        return '<Stats %s / %r / %s >' % ( self.key, self.val, self.val_s )
-
-    def save(self):
-
-        db_obj = Stats.query.filter_by(key=self.key).first()
-
-        # update the existing db object
-        if db_obj:
-
-            db_obj.val   = self.val
-            db_obj.val_s = self.val_s
-
-        # commit change and save the object
-        db.session.commit( )
-
-        return self
--- run.py
+++ run.py
@@ -1,11 +1,7 @@
 # -*- encoding: utf-8 -*-
 """
-Argon Dashboard - coded in Flask
-
-Author  : AppSeed App Generator
-Design  : Creative-Tim.com
-License : MIT 
-Support : https://appseed.us/support 
+License: MIT
+Copyright (c) 2019 - present AppSeed.us
 """
 
 # used by the static export
