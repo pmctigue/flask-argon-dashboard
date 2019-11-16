f900dbc538e9116a91d1b7fa9c2c2ba71eb39f9f
app-generator <app-generator@appseed.us>
2019-11-16 14:24:51 +0200

    code cleanup

--- run.py
+++ run.py
@@ -4,20 +4,4 @@ License: MIT
 Copyright (c) 2019 - present AppSeed.us
 """
 
-# used by the static export
-import click
-from   flask_frozen import Freezer
-
-from app import app
-from app import db
-
-# define custom command 
-@app.cli.command()
-def build():
-    freezer = Freezer(app)
-    freezer.freeze()
-
-if __name__ == "__main__":
-
-    db.create_all()
-    app.run() 
+from app import app, db
